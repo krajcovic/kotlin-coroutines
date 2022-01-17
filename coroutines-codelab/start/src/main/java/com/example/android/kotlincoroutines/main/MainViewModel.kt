999fc68bf9ebcc6ee2e915734459cdfbcfa0faa5
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2022-01-17 11:08:11 +0100

    Coroutines in room and retrofit

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,8 +19,11 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -102,11 +105,30 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      */
     private fun updateTaps() {
         // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
-            _taps.postValue("${tapCount} taps")
+        val version = 2
+        when (version) {
+            0 -> {
+                tapCount++
+                BACKGROUND.submit {
+                    Thread.sleep(1_000)
+                    _taps.postValue("${tapCount} taps")
+                }
+            }
+            1 -> {
+                tapCount++
+                Thread.sleep(1_000)
+                _taps.postValue("${tapCount} taps")
+
+            }
+            2 -> {
+                viewModelScope.launch {
+                    tapCount++
+                    delay(1_000)
+                    _taps.postValue("${tapCount} taps")
+                }
+            }
         }
+
     }
 
     /**
@@ -121,16 +143,26 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      */
     fun refreshTitle() {
         // TODO: Convert refreshTitle to use coroutines
-        _spinner.value = true
-        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
-            override fun onCompleted() {
-                _spinner.postValue(false)
+//        _spinner.value = true
+//        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
+//            override fun onCompleted() {
+//                _spinner.postValue(false)
+//            }
+//
+//            override fun onError(cause: Throwable) {
+//                _snackBar.postValue(cause.message)
+//                _spinner.postValue(false)
+//            }
+//        })
+        viewModelScope.launch {
+            try {
+                _spinner.value = true
+                repository.refreshTitle()
+            } catch (error: TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
             }
-
-            override fun onError(cause: Throwable) {
-                _snackBar.postValue(cause.message)
-                _spinner.postValue(false)
-            }
-        })
+        }
     }
 }
